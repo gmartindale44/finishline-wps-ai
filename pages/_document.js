e4c3699ecf8f8b2a599710522068b8e70ed3de4c
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-11-10 14:35:20 -0600

    feat(verify): add stable verify tab in predictions modal

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script defer src="/js/verify-tab.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
