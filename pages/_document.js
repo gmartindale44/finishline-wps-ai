905c1248ee2ccf9bb70d31986bf5d53de0b5d847
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-11-10 15:53:25 -0600

    feat(verify): add router-agnostic loader + heartbeat badge

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  // bump to force reload
-  const VER_TAB_VERSION = "v2025-11-10-2";
   return (
     <Html>
       <Head />
       <body>
         <Main />
         <NextScript />
-        <script defer src={`/js/verify-tab.js?${VER_TAB_VERSION}`}></script>
       </body>
     </Html>
   );
