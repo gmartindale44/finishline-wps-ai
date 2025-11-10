ba2c744ac28ec88cfcbbfdd92f429bfa9674b4ec
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-11-10 15:07:02 -0600

    fix(verify): cache-bust verify tab and mount in modal

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // bump this when you want to force cache busting
+  const VER_TAB_VERSION = "v2025-11-10-1";
   return (
     <Html>
-      <Head>
-        <script defer src="/js/verify-tab.js"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <script
+          defer
+          src={`/js/verify-tab.js?${VER_TAB_VERSION}`}
+        ></script>
       </body>
     </Html>
   );
