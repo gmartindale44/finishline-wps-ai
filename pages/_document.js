4806c4ce867d35a956c8dfa87658a4f38d4c6443
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-11-10 16:28:15 -0600

    feat(verify): r5 loader via _app/_document with basePath support

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <Script
+            id="fl-verify-loader-backstop"
+            src="/js/verify-loader.js?v=v2025-11-10-6"
+            strategy="afterInteractive"
+          />
+        </body>
+      </Html>
+    );
+  }
 }
