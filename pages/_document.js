8102f157daaec57a5e5e3f0bd5996c8b631c2b1a
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-11-10 19:46:41 -0600

    feat(verify): r13 modal sends track/date + raceNo hints

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
           <NextScript />
           <Script
             id="fl-verify-loader-backstop"
-            src="/js/verify-loader.js?v=v2025-11-10-12"
+            src="/js/verify-loader.js?v=v2025-11-10-13"
             strategy="afterInteractive"
           />
         </body>
