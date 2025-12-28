dbdf022bce666a56e266875417490ebef9884279
gmartindale44 <@https://github.com/gmartindale44/hiredhive-site.git>
2025-12-28 10:48:35 -0600

    feat: add family unlock link with env var token

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="fl-env-token"
+        dangerouslySetInnerHTML={{
+          __html: `window.__FL_FAMILY_UNLOCK_TOKEN__ = ${JSON.stringify(process.env.FAMILY_UNLOCK_TOKEN || null)};`,
+        }}
+        strategy="beforeInteractive"
+      />
       <Script id="fl-verify-loader" src="/js/verify-loader.js?v=v2025-11-15-verify-final" strategy="afterInteractive" />
       <Component {...pageProps} />
     </>
